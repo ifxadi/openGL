e39725eea977c4942111a45ddceb577dae0d998d
ifxadi <adityakumarteli7846@gmail.com>
2025-08-17 20:08:26 +0530

    Update viewport color to dark

--- main.cpp
+++ main.cpp
@@ -220,7 +220,7 @@ void PreDraw() {
 	glDisable(GL_CULL_FACE);
 
 	glViewport(0, 0, gScreenWidth, gScreenHeight);
-	glClearColor(1.f, 1.f, 0.f, 1.f);
+	glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
 	glUseProgram(gGraphicsPipelineShaderProgram);
